e8ff0f62e2c844fe2e1439c00919f33772d1b43b
Clulnes <art.yusupov2003@yandex.ru>
2025-12-19 22:59:41 +0300

    Финальный вариант ТЗ-9

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,5 +1,43 @@
 package ru.practicum.moviehub.model;
 
-public class Movie {
+import java.util.Objects;
 
+public class Movie {
+    private long id;
+    private final String title;
+    private final int year;
+
+    public Movie(int year, String title, long id) {
+        this.year = year;
+        this.title = title;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return getId() == movie.getId() && getYear() == movie.getYear() && Objects.equals(getTitle(), movie.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getYear());
+    }
 }
